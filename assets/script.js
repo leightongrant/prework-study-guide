945d6c172052462615167bc08402e4ffbba6c096
misterouija <17514172+misterouija@users.noreply.github.com>
2022-11-02 23:35:50 +0000

    added comments to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
-let topic = "HTML";
+// let topic = "HTML";
 
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
-}
+// if (topic === 'HTML') {
+//     console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//     console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//     console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//     console.log("Let's study JavaScript!");
+// } else {
+//     console.log('Please try again!');
+// }
 
 let shapes = ["triangle", "square", "pentagon", "circle"];
 let idx;
